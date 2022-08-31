f986eee296b0aecd54e1a09eb1c5116487761926
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-08-31 23:14:35 +0100

    doc

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import AgentProfile, HomeProfile, Profile, TrainingCertificates, User
+from .models import AgentProfile, Docs, HomeProfile, Profile, TrainingCertificates, User
 
 # Register your models here.
 
-admin.site.register((User, Profile, HomeProfile, TrainingCertificates, AgentProfile ))
+admin.site.register((User, Profile, HomeProfile, TrainingCertificates, AgentProfile, Docs))
