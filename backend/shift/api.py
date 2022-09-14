ed9e5d009c57f8c7592236af3fe92db152b0dc1f
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-09-14 18:09:03 +0100

    doc

--- backend/shift/api.py
+++ backend/shift/api.py
@@ -79,9 +79,9 @@ class NotificationListApi(generics.GenericAPIView):
         data = request.GET
         qs1 = Notifications.objects.all()
         qs2 = HomeNotifications.objects.all()
-        print(qs1[0].employee, qs2, "anandhu")
         ser_1 = NotificationSerializer(qs1, many=True, context={"employee_id":False, "shift_ass_id":False}).data
         ser_2 = HomeNotificationSerializer(qs2, many=True, context={"employee_id":False, "shift_ass_id":False}).data
+        print(ser_1, ser_2, "sererererer")
         final_list = sorted(chain(ser_1, ser_2), key=attrgetter('date_added'))
         return Response(NotificationSerializer(final_list, many=True, context={"employee_id":False, "shift_ass_id":False}).data, status=200)
         
