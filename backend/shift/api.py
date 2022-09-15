305d8749fd82a99ee06d6da458f402eb9f6dec66
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-09-15 14:01:10 +0100

    doc

--- backend/shift/api.py
+++ backend/shift/api.py
@@ -143,7 +143,11 @@ def CancelRequest(request, *args, **kwargs):
         ass = ShiftAssignment.objects.get(id=shift_ass_id)
         shift = ShiftName.objects.get(id=shift_id)
         noti = Notifications.objects.get_or_create(shift=shift, type=1, body=data["body"],dealt=False, employee=emp, shift_ass=ass)
-        return Response(NotificationSerializer(noti, context={"employee_id":employee_id, "shift_ass_id":shift_ass_id}).data, status=200)
+        print(type(noti), "type of noti")
+        if type(noti)!="Tuple":
+            return Response(NotificationSerializer(noti, context={"employee_id":employee_id, "shift_ass_id":shift_ass_id}).data, status=200)
+        else:
+            return Response(NotificationSerializer(noti[0], context={"employee_id":employee_id, "shift_ass_id":shift_ass_id}).data, status=200)
 
 @api_view(["POST"])
 def WriteTimesheet(request, *args, **kwargs):
