9728bb9cc59ad73e11e5bf4796a3a8275dc023bb
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-09-19 18:47:05 +0100

    doc

--- backend/accounts/serializer.py
+++ backend/accounts/serializer.py
@@ -46,6 +46,7 @@ class DocumentSerializer(serializers.ModelSerializer):
       if obj.name == ins.name:
         flag["check"] = True
         flag["id"] = obj.id
+        print(obj.file, "file")
         flag["file"] = obj.file
     return flag
 
