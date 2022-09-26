6e2fc3dd676356c8d80e768bc9e455dda1c78baa
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-09-26 14:10:53 +0100

    new

--- backend/accounts/api.py
+++ backend/accounts/api.py
@@ -88,6 +88,7 @@ class LoginAPI(generics.GenericAPIView):
     push_token = request.data["push_token"]
     serializer = self.get_serializer(data={"email":email, "password":password})    
     serializer.is_valid(raise_exception=True)
+    print(serializer.errors, "aopop")
     user = serializer.validated_data
     _, token = AuthToken.objects.create(user)
     user.push_token = push_token
